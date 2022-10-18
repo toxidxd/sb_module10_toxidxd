55389d8b3d72a6e413071c48e70891aad3fed73b
toxidxd <toxid13@yandex.ru>
2022-10-18 14:32:41 +0300

    done

--- task_9.py
+++ task_9.py
@@ -6,8 +6,39 @@ print('Задача 9. Пирамидка 2')
 
 # Пример:
 # 
-#             1
-#          3     5
-#       7     9     11
+#             11
+#          31    51
+#       71    91    11
 #    13    15    17    19
 # 21    23    25    27    29
+
+pyramid_height = int(input("Введите высоту пирамиды: "))
+pyramid_width = 1
+pyramid_level = 1
+number = 1
+
+for level in range(pyramid_height - 1):
+    pyramid_width += 2
+
+for level in range(1, pyramid_height + 1):
+    free_space = (pyramid_width - pyramid_level) // 2
+    # print(" " * free_space + "#" * pyramid_level + " " * free_space)
+    for _ in range(free_space):
+        print("  ", end="")
+
+    for _ in range(pyramid_level):
+        if number % 2 != 0:
+            if number < 10:
+                print(number, end=" ")
+            else:
+                print(number, end="")
+        else:
+            print(" ", end=" ")
+        number += 1
+
+    for _ in range(free_space):
+        print(" ", end="")
+
+    number += 1
+    pyramid_level += 2
+    print()

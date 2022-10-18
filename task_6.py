9d59ac4bc6a6240bc9288958f68db26b788ff5a7
toxidxd <toxid13@yandex.ru>
2022-10-18 13:27:25 +0300

    done

--- task_6.py
+++ task_6.py
@@ -3,3 +3,18 @@ print('Задача 6. Сумма факториалов')
 # Напишите программу,
 # которая запрашивает у пользователя число N
 # и находит сумму факториалов 1! + 2! + 3! +... + N! 
+
+num_seq_size = int(input("Введите количество чисел: "))
+fact_sum = 0
+
+for num in range(1, num_seq_size + 1):
+	print(f"{num}! = ", end="")
+	cur_fact = 1
+
+	for x in range(1, num + 1):
+		cur_fact *= x
+
+	print(cur_fact)
+	fact_sum += cur_fact
+
+print(f"Сумма факториалов: {fact_sum}")

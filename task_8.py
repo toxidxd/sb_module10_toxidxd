04438cf0982320e52cc63eede754cff470a4a076
toxidxd <toxid13@yandex.ru>
2022-10-18 14:06:38 +0300

    done

--- task_8.py
+++ task_8.py
@@ -12,3 +12,15 @@ print('Задача 8. Пирамидка')
   ###
  #####
 #######
+
+pyramid_height = int(input("Введите высоту пирамиды: "))
+pyramid_width = 1
+pyramid_level = 1
+
+for level in range(pyramid_height - 1):
+    pyramid_width += 2
+
+for level in range(1, pyramid_height + 1):
+    free_space = (pyramid_width - pyramid_level) // 2
+    print(" " * free_space + "#" * pyramid_level + " " * free_space)
+    pyramid_level += 2
